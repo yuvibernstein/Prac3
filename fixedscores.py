a2be7d4cb8e70a98c28f3ed874179d37d06c28fe
jc258588 <yuval.bernstein@my.jcu.edu.au>
2016-08-16 13:59:45 +1000

    added 2 new practise questions

---
+++ fixedscores.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+# TODO: Fix this!
+
+score = float(input("Enter score: "))
+while score < 0 or score > 100:
+    print("Invalid score")
+    if score > 50:
+        print("Passable")
+    if score > 90:
+        print("Excellent")
+    if score < 50:
+        print("Bad")
---
+++ temperatures.py
@@ -0,0 +1,26 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+__author__ = 'Lindsay Ward'
+
+MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
+print(MENU)
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = celsius * 9.0 / 5 + 32
+        print("Result: {:.2f} F".format(fahrenheit))
+    elif choice == "F":
+        fahrenheit = float(input("fahrenheit: "))
+        celsius = 5 / 9 * (fahrenheit - 32)
+        print("Result: {:.2f} F".format(celsius))
+        # TODO: Write this section so the program converts F to C and displays the result
+        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+        pass
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
