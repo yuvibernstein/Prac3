842555a9ee7bd5ad222bd4106e69ca1a683f0a94
jc258588 <yuval.bernstein@my.jcu.edu.au>
2016-08-16 13:38:51 +1000

    Finished stock_price.py

---
+++ asciiTable.py
@@ -0,0 +1,22 @@
+def main():
+    lower = 10
+    upper = 150
+    number = get_num(lower, upper)
+    print(chr(number))
+
+def get_num(lower,upper):
+    valid_input = False
+    while not valid_input:
+        try:
+            number = int(input("enter a number ({0}-{1}):".format(lower, upper)))
+            if lower <= number <= upper:
+                valid_input = True
+            else:
+                print("Enter a valid number")
+        except ValueError:
+            print("Please enter a valid integer.")
+        except
+    return number
+
+
+main()
